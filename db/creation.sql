bd489e921672bbf2cbde39968b6372722c3a2a10
Jonah Greenthal <jonah@jonahgreenthal.com>
2017-10-03 21:01:51 -0500

    add some indexes and drop an unused table

--- db/creation.sql
+++ db/creation.sql
@@ -39,6 +39,8 @@ CREATE TABLE Tournament (
 	questions_complete BOOLEAN NOT NULL DEFAULT FALSE,
 	question_download_url TEXT
 );
+CREATE INDEX tournament_tdcontact_idx ON Tournament(tournament_director_contact_id);
+CREATE INDEX tournament_tcontrolroom_idx ON Tournament(control_room_room_id);
 
 CREATE TABLE Account (
 	id INTEGER PRIMARY KEY REFERENCES Contact ON UPDATE CASCADE ON DELETE RESTRICT,
@@ -51,6 +53,7 @@ CREATE TABLE Account (
 	active BOOLEAN NOT NULL DEFAULT TRUE,
 	can_receive_unsolicited_messages BOOLEAN NOT NULL DEFAULT FALSE
 );
+CREATE INDEX account_contact_idx ON Account(id);
 
 CREATE TABLE School (
 	id SERIAL PRIMARY KEY,
@@ -75,6 +78,9 @@ CREATE TABLE School_Registration (
 	UNIQUE(school_id, tournament_code)
 );
 ALTER SEQUENCE school_registration_id_seq RESTART WITH 1000;
+CREATE INDEX schoolregistration_school_idx ON School_Registration(school_id);
+CREATE INDEX schoolregistration_tournament_idx ON School_Registration(tournament_code);
+CREATE INDEX schoolregistration_maincontact_idx ON School_Registration(main_contact_id);
 
 CREATE TABLE Waitlist_Entry (
 	id SERIAL PRIMARY KEY,
@@ -84,6 +90,7 @@ CREATE TABLE Waitlist_Entry (
 	note note_t
 );
 ALTER SEQUENCE waitlist_entry_id_seq RESTART WITH 1000;
+CREATE INDEX waitlistentry_schoolregistration_idx ON Waitlist_Entry(school_registration_id);
 
 CREATE TABLE Standby_Entry (
 	id SERIAL PRIMARY KEY,
@@ -93,6 +100,7 @@ CREATE TABLE Standby_Entry (
 	note note_t
 );
 ALTER SEQUENCE standby_entry_id_seq RESTART WITH 1000;
+CREATE INDEX standbyentry_schoolregistration_idx ON Standby_Entry(school_registration_id);
 
 CREATE TABLE Technology_Choice (
 	code code_t PRIMARY KEY,
@@ -116,6 +124,12 @@ CREATE TABLE Staff (
 	note note_t,
 	technology_choice_code code_t REFERENCES Technology_Choice ON UPDATE CASCADE ON DELETE RESTRICT DEFAULT 'TBD'
 );
+ALTER SEQUENCE staff_id_seq RESTART WITH 1000;
+CREATE INDEX staff_contact_idx ON Staff(contact_id);
+CREATE INDEX staff_tournament_idx ON Staff(tournament_code);
+CREATE INDEX staff_schoolregistration_idx ON Staff(school_registration_id);
+CREATE INDEX staff_technologychoice_idx ON Staff(technology_choice_code);
+
 
 CREATE TABLE Staff_Role (
 	code code_t PRIMARY KEY,
@@ -143,6 +157,10 @@ CREATE TABLE Staff_Assignment (
 	phase_id INTEGER NOT NULL REFERENCES Phase ON UPDATE CASCADE ON DELETE RESTRICT,
 	staff_role_code code_t REFERENCES Staff_Role ON UPDATE CASCADE ON DELETE RESTRICT
 );
+CREATE INDEX staffassignment_staff_idx ON Staff_Assignment(staff_id);
+CREATE INDEX staffassignment_room_idx ON Staff_Assignment(room_id);
+CREATE INDEX staffassignment_phase_idx ON Staff_Assignment(phase_id);
+CREATE INDEX staffassignment_staffrole_idx ON Staff_Assignment(staff_role_code);
 
 CREATE TABLE School_Year (
 	code code_t PRIMARY KEY,
@@ -170,8 +188,9 @@ CREATE TABLE Player (
 	note note_t
 );
 ALTER SEQUENCE player_id_seq RESTART WITH 1000;
-CREATE INDEX player_schoolregistration_idx ON Player(school_registration_id);
 CREATE INDEX player_contact_idx ON Player(contact_id);
+CREATE INDEX player_schoolregistration_idx ON Player(school_registration_id);
+CREATE INDEX player_schoolyear_idx ON Player(school_year_code);
 
 
 CREATE TABLE Room (
@@ -186,6 +205,7 @@ CREATE TABLE Room (
 	UNIQUE(tournament_code, short_name)
 );
 ALTER SEQUENCE room_id_seq RESTART WITH 1000;
+CREATE INDEX room_tournament_idx ON Room(tournament_code);
 
 CREATE TABLE Buzzer (
 	id SERIAL PRIMARY KEY,
@@ -193,6 +213,8 @@ CREATE TABLE Buzzer (
 	room_id INTEGER REFERENCES Room ON UPDATE CASCADE ON DELETE RESTRICT, -- also implies tournament
 	label VARCHAR(256)
 );
+CREATE INDEX buzzer_schoolregistration_idx ON Buzzer(school_registration_id);
+CREATE INDEX buzzer_room_idx ON Buzzer(room_id);
 
 CREATE TABLE Phase (
 	id SERIAL PRIMARY KEY,
@@ -206,6 +228,7 @@ CREATE TABLE Phase (
 	UNIQUE(tournament_code, short_name)
 );
 ALTER SEQUENCE phase_id_seq RESTART WITH 1000;
+CREATE INDEX phase_tournament_idx ON Phase(tournament_code);
 
 CREATE TABLE Round_Group (
 	id SERIAL PRIMARY KEY,
@@ -242,10 +265,11 @@ CREATE TABLE Card (
 	short_name short_name_t,
 	sequence sequence_t,
 	UNIQUE(initial_player_id),
-	UNIQUE(tournament_code, name),
-	UNIQUE(tournament_code, short_name)
+	UNIQUE(phase_id, name),
+	UNIQUE(phase_id, short_name)
 );
 ALTER SEQUENCE card_id_seq RESTART WITH 1000;
+CREATE INDEX card_phase_idx ON Card(phase_id);
 
 CREATE TABLE Match (
 	id SERIAL PRIMARY KEY,
@@ -258,6 +282,7 @@ CREATE TABLE Match (
 );
 ALTER SEQUENCE match_id_seq RESTART WITH 1000;
 CREATE INDEX match_round_idx ON Match(round_id);
+CREATE INDEX match_room_idx ON Match(room_id);
 
 CREATE TABLE Game (-- 1-1 with Match
 	id INTEGER PRIMARY KEY REFERENCES Match ON UPDATE CASCADE ON DELETE RESTRICT,
@@ -271,6 +296,13 @@ CREATE TABLE Game (-- 1-1 with Match
 	CHECK(incoming_winning_card_player_id IS NOT NULL OR incoming_losing_card_player_id IS NOT NULL),
 	CHECK((outgoing_winning_card_player_id IS NULL AND outgoing_losing_card_player_id IS NULL) OR (incoming_winning_card_player_id = outgoing_winning_card_player_id AND incoming_losing_card_player_id = outgoing_losing_card_player_id) OR (incoming_winning_card_player_id = outgoing_losing_card_player_id AND incoming_losing_card_player_id = outgoing_winning_card_player_id)) 
 );
+CREATE INDEX game_id_idx ON Game(id);
+CREATE INDEX game_moderatorstaff_idx ON Game(moderator_staff_id);
+CREATE INDEX game_scorekeeperstaff_idx ON Game(moderator_staff_id);
+CREATE INDEX game_incomingwinningcardplayer_idx ON Game(incoming_winning_card_player_id);
+CREATE INDEX game_incominglosingcardplayer_idx ON Game(incoming_losing_card_player_id);
+CREATE INDEX game_outgoingwinningcardplayer_idx ON Game(outgoing_winning_card_player_id);
+CREATE INDEX game_outgoinglosingcardplayer_idx ON Game(outgoing_losing_card_player_id);
 
 CREATE TABLE Performance (
 	id SERIAL PRIMARY KEY,
@@ -298,6 +330,7 @@ CREATE TABLE Category (
 	category_group_code code_t REFERENCES Category_Group ON UPDATE CASCADE ON DELETE RESTRICT,
 	allow_pronunciation_guide_suppression BOOLEAN NOT NULL DEFAULT TRUE
 );
+CREATE INDEX category_categorygroup_idx ON Category(category_group_code);
 
 CREATE TABLE Category_Use (
 	category_code code_t REFERENCES Category ON UPDATE CASCADE ON DELETE RESTRICT,
@@ -305,6 +338,8 @@ CREATE TABLE Category_Use (
 	needs INTEGER CHECK (needs IS NULL OR needs > 0),
 	PRIMARY KEY (category_code, tournament_code)
 );
+CREATE INDEX categoryuse_category_idx ON Category_Use(category_code);
+CREATE INDEX categoryuse_tournament_idx ON Category_Use(tournament_code);
 
 CREATE TABLE Question (
 	id SERIAL PRIMARY KEY,
@@ -318,6 +353,8 @@ CREATE TABLE Question (
 );
 ALTER SEQUENCE question_id_seq RESTART WITH 1000;
 CREATE INDEX question_category_idx ON Question(category_code);
+CREATE INDEX question_writeraccount_idx ON Question(writer_account_id);
+CREATE INDEX question_questionstatus_idx ON Question(question_status_code);
 
 CREATE TABLE Question_Status (
 	code code_t PRIMARY KEY,
@@ -348,15 +385,10 @@ CREATE TABLE Diff (
 	UNIQUE(question_id, revision_number)
 );
 ALTER SEQUENCE diff_id_seq RESTART WITH 1000;
-
-CREATE TABLE Approval (
-	id SERIAL PRIMARY KEY,
-	diff_id INTEGER NOT NULL REFERENCES Diff ON UPDATE CASCADE ON DELETE RESTRICT,
-	approver_account_id INTEGER NOT NULL REFERENCES Account ON UPDATE CASCADE ON DELETE RESTRICT,
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	UNIQUE(diff_id, approver_account_id)
-);
-ALTER SEQUENCE approval_id_seq RESTART WITH 1000;
+CREATE INDEX diff_question_idx ON Diff(question_id);
+CREATE INDEX diff_editoraccount_idx ON Diff(editor_account_id);
+CREATE INDEX diff_questionstatus_idx ON Diff(question_status_code);
+CREATE INDEX diff_category_idx ON Diff(category_code);
 
 CREATE TABLE Packet (
 	id SERIAL PRIMARY KEY,
@@ -370,6 +402,9 @@ CREATE TABLE Packet (
 	questions_public BOOLEAN NOT NULL DEFAULT FALSE
 );
 ALTER SEQUENCE packet_id_seq RESTART WITH 1000;
+CREATE INDEX packet_tournament_idx ON Packet(tournament_code);
+CREATE INDEX packet_round_idx ON Packet(round_id);
+CREATE INDEX packet_replacementpacket_idx ON Packet(replacement_packet_id);
 
 CREATE TABLE Placement (
 	id SERIAL PRIMARY KEY,
@@ -384,6 +419,7 @@ CREATE TABLE Placement (
 ALTER SEQUENCE placement_id_seq RESTART WITH 1000;
 CREATE INDEX placement_question_idx ON Placement(question_id);
 CREATE INDEX placement_packet_idx ON Placement(packet_id);
+CREATE INDEX placement_category_idx ON Placement(category_code);
 
 CREATE VIEW Placement_v AS
 SELECT
@@ -427,6 +463,8 @@ ALTER SEQUENCE response_id_seq RESTART WITH 1000;
 CREATE INDEX response_diff_idx ON Response(diff_id);
 CREATE INDEX response_performance_idx ON Response(performance_id);
 CREATE INDEX response_responsetype_idx ON Response(response_type_code);
+CREATE INDEX response_baseplacement_idx ON Response(base_placement_id);
+CREATE INDEX response_replacementplacement_idx ON Response(replacement_placement_id);
 
 CREATE TABLE Pronunciation_Guide_Suppression (
 	id SERIAL PRIMARY KEY,
@@ -435,6 +473,8 @@ CREATE TABLE Pronunciation_Guide_Suppression (
 	UNIQUE(account_id, category_code)
 );
 ALTER SEQUENCE pronunciation_guide_suppression_id_seq RESTART WITH 1000;
+CREATE INDEX pronunciationguidesuppression_account_idx ON Pronunciation_Guide_Suppression(account_id);
+CREATE INDEX pronunciationguidesuppression_category_idx ON Pronunciation_Guide_Suppression(category_code);
 
 CREATE TABLE Message (
 	id SERIAL PRIMARY KEY,
@@ -446,6 +486,8 @@ CREATE TABLE Message (
 	archived BOOLEAN NOT NULL DEFAULT FALSE
 );
 ALTER SEQUENCE message_id_seq RESTART WITH 1000;
+CREATE INDEX message_fromaccount_idx ON Message(from_account_id);
+CREATE INDEX message_toaccount_idx ON Message(to_account_id);
 
 
 
