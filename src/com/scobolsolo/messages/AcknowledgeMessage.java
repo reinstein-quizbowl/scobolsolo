b2d391edf057d79463e18fd29dfb8cedf9332b26
Jonah Greenthal <jonah@jonahgreenthal.com>
2017-10-28 12:10:29 -0500

    Fixes #62, at least in the sense of quieting things down

--- src/com/scobolsolo/messages/AcknowledgeMessage.java
+++ src/com/scobolsolo/messages/AcknowledgeMessage.java
@@ -19,21 +19,25 @@ import com.scobolsolo.servlets.AjaxServlet;
 
 public class AcknowledgeMessage extends AjaxServlet {
 	private static final long serialVersionUID = 1L;
+	private static final org.apache.log4j.Logger ourLogger = org.apache.log4j.Logger.getLogger(AcknowledgeMessage.class.getName());
 	
 	@Override
 	protected JsonObject process(final HttpServletRequest argRequest, final HttpServletResponse argResponse, final Account argUser) {
-		final Collection<Message> lclMessages = Validate.notEmpty(MessageFactory.getInstance().multipleFromHttpRequest(argRequest));
-		
-		Validate.isTrue(argUser.isAdministrator() || lclMessages.stream().allMatch(argM -> argM.getRecipient() == argUser), "Not authorized");
-		
-		final LocalDateTime lclNow = LocalDateCache.now();
-		
-		try (TransactionContext lclTC = TransactionContext.createAndActivate()) {
-			for (final Message lclM : lclMessages) {
-				lclM.setAcknowledgedTimestamp(lclNow);
-			}
+		final Collection<Message> lclMessages = MessageFactory.getInstance().multipleFromHttpRequest(argRequest);
+		if (lclMessages.isEmpty()) {
+			ourLogger.warn("AcknowledgeMessage called with no messages to acknowledge");
+		} else {
+			Validate.isTrue(argUser.isAdministrator() || lclMessages.stream().allMatch(argM -> argM.getRecipient() == argUser), "Not authorized");
 			
-			lclTC.complete();
+			final LocalDateTime lclNow = LocalDateCache.now();
+			
+			try (TransactionContext lclTC = TransactionContext.createAndActivate()) {
+				for (final Message lclM : lclMessages) {
+					lclM.setAcknowledgedTimestamp(lclNow);
+				}
+				
+				lclTC.complete();
+			}
 		}
 		
 		return null; // I guess we could say something vaguely meaningful, like just spitting back the Message IDs
