6fa0d50a92ccd3f07a4ff75b135caaadb06fe14b
Jonah Greenthal <jonah@jonahgreenthal.com>
2018-09-13 14:12:30 -0500

    don't confuse an account with itself

--- src/com/scobolsolo/opalforms/validator/AccountValidator.java
+++ src/com/scobolsolo/opalforms/validator/AccountValidator.java
@@ -2,6 +2,8 @@ package com.scobolsolo.opalforms.validator;
 
 import com.opal.cma.Validator;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.scobolsolo.application.Account;
 import com.scobolsolo.application.AccountFactory;
 
@@ -10,14 +12,15 @@ public class AccountValidator extends Validator<Account> {
 		super();
 	}
 	
-	public void validateUsername(final String argUsername) {
-		if (argUsername == null) {
-			return;
-		}
-		
-		final Account lclUsernameDuplicate = AccountFactory.getInstance().forUsername(argUsername.trim());
-		if (lclUsernameDuplicate != null) {
-			addError("Username", "There is already an account, " + lclUsernameDuplicate.getContact().getName() + ", with username " + argUsername);
+	public void validate(final Account argA) {
+		if (argA != null) {
+			final String lclUsername = StringUtils.trimToNull(argA.getUsername());
+			if (lclUsername != null) {
+				final Account lclUsernameDuplicate = AccountFactory.getInstance().forUsername(lclUsername);
+				if (lclUsernameDuplicate != null && lclUsernameDuplicate != argA) {
+					addError("Username", "There is already an account, " + lclUsernameDuplicate.getContact().getName() + ", with username " + lclUsername);
+				}
+			}
 		}
 	}
 }
