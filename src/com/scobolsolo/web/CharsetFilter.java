568a2e757755552e2fea5dbdeb7c21525c39ff06
Jonah Greenthal <jonah@jonahgreenthal.com>
2019-10-31 21:01:10 -0500

    don't mess with downloads' encodings

--- src/com/scobolsolo/web/CharsetFilter.java
+++ src/com/scobolsolo/web/CharsetFilter.java
@@ -1,6 +1,7 @@
 package com.scobolsolo.web;
 
 import java.io.IOException;
+import java.util.Set;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterConfig;
@@ -8,16 +9,20 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.Validate;
+
+import com.google.common.collect.ImmutableSet;
 
 /**
  * @author jonah
  * Semi-copied from http://stackoverflow.com/questions/138948/how-to-get-utf-8-working-in-java-webapps
  */
 public class CharsetFilter implements Filter {
-	private static final org.apache.log4j.Logger ourLogger = org.apache.log4j.Logger.getLogger(CharsetFilter.class.getName());
 	public static final String DEFAULT_ENCODING = "UTF-8";
+	private static final Set<String> EXCLUDES = ImmutableSet.of("/tournament/GeneratePaperwork", "/tournament/OutputPackets");
+	// private static final org.apache.log4j.Logger ourLogger = org.apache.log4j.Logger.getLogger(CharsetFilter.class.getName());
 	
 	@Override
 	public void init(FilterConfig argConfig) throws ServletException {
@@ -30,12 +35,14 @@ public class CharsetFilter implements Filter {
 		Validate.notNull(argResponse);
 		Validate.notNull(argChain);
 		
-		// Respect the client-specified character encoding (see HTTP specification section 3.4.1)
-		if (argRequest.getCharacterEncoding() == null) {
-			argRequest.setCharacterEncoding(DEFAULT_ENCODING);
+		if (applyTo(argRequest)) {
+			// Respect the client-specified character encoding (see HTTP specification section 3.4.1)
+			if (argRequest.getCharacterEncoding() == null) {
+				argRequest.setCharacterEncoding(DEFAULT_ENCODING);
+			}
+			
+			argResponse.setCharacterEncoding(DEFAULT_ENCODING);
 		}
-		
-		argResponse.setCharacterEncoding(DEFAULT_ENCODING);
 		
 		argChain.doFilter(argRequest, argResponse);
 	}
@@ -43,6 +50,16 @@ public class CharsetFilter implements Filter {
 	@Override
 	public void destroy() {
 		return;
+	}
+	
+	private static final boolean applyTo(ServletRequest argRequest) {
+		if (argRequest instanceof HttpServletRequest) {
+			HttpServletRequest lclRequest = (HttpServletRequest) argRequest;
+			
+			return EXCLUDES.contains(lclRequest.getRequestURI()) == false;
+		} else {
+			return true;
+		}
 	}
 }
 
